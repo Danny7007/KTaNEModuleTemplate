60e18e59609942022f871c4f7039008f6af3daa9
Danny7007 <67610733+Danny7007@users.noreply.github.com>
2021-09-01 18:31:18 -0400

    Fix PickRandom, rename BounceLerp, add more methods
    - Fix PickRandom so that it will no longer never pick the last item.
    - Renamed BounceLerp to InOutLerp
    - Add CapitalizeFirst as a string extension.
    - Add SplitCaps as a string extension
    - Added LogGrid to log rectangular grids for KTaNE purposes
    - Added LogIntegerGrid also for KTaNE purposes.

--- Module Template/Assets/Scripts/Ut.cs
+++ Module Template/Assets/Scripts/Ut.cs
@@ -120,7 +120,7 @@ public static class Ut {
         IEnumerable<T> filteredCollection = collection.Where(condition);
         if (filteredCollection.Count() == 0)
             throw new InvalidOperationException("Filtered set contains no items.");
-        return filteredCollection.ElementAt(Rnd.Range(0, filteredCollection.Count() - 1));
+        return filteredCollection.ElementAt(Rnd.Range(0, filteredCollection.Count()));
     }
     /// <summary>
     /// Chooses a random item from <paramref name="collection"/>.
@@ -133,7 +133,7 @@ public static class Ut {
             throw new ArgumentNullException("collection");
         if (collection.Count() == 0)
             throw new InvalidOperationException("Cannot pick an element from an empty set.");
-        return collection.ElementAt(Rnd.Range(0, collection.Count() - 1));
+        return collection.ElementAt(Rnd.Range(0, collection.Count()));
     }
 
     /// <summary>
@@ -188,7 +188,7 @@ public static class Ut {
     /// <param name="end">The end value to be interpolated to. This value will be equal to the output when <paramref name="t"/></param>
     /// <param name="t">The interpolant; within 0.0 to 1.0.</param>
     /// <returns>The result of the interpolation.</returns>
-    public static float BounceLerp(float start, float end, float t)
+    public static float InOutLerp(float start, float end, float t)
     {
         if (t < 0)
             t = 0;
@@ -198,4 +198,83 @@ public static class Ut {
             return Mathf.Lerp(start, end, t * 2);
         else return Mathf.Lerp(end, start, t * 2 - 1);
     }
+    /// <summary>
+    ///     Returns the value of <paramref name="str"/> with its first character capitalized
+    /// </summary>
+    /// <param name="str">The string to have its first letter capitalized.</param>
+    public static string CapitalizeFirst(this string str)
+    {
+        return char.ToUpper(str[0]) + str.Substring(1);
+    }
+
+    /// <summary>
+    ///     Takes <paramref name="str"/> and inserts spaces before all capital letters other than the first.
+    /// </summary>
+    /// <param name="str">The string to be split</param>
+    /// <returns></returns>
+    public static string SplitCaps(this string str)
+    {
+        string output = "";
+        foreach (char letter in str)
+        {
+            if (char.IsUpper(letter))
+                output += ' ';
+            output += letter;
+        }
+        return output.TrimStart(' ');
+    }
+
+    /// <summary>
+    ///     Decomposes a rectangular grid into multiple rows and sends each as a log message such that the logging is compatible with the Logfile Analyzer.
+    /// </summary>
+    /// <param name="loggingTag">The module's display name and moduleId, formatted as "[{displayName} #{moduleId}]"</param>
+    /// <param name="grid">The grid to be logged. The grid must be rectangular.</param>
+    /// <param name="height">The number of rows in the grid. The output will consist of this many messages.</param>
+    /// <param name="width">The number of items per row of the grid.</param>
+    /// <param name="separator">A string to separate each entry of the grid</param>
+    /// <exception cref="ArgumentOutOfRangeException">Occurs when the stated width or height of the grid is 0 or fewer.</exception>
+    /// <exception cref="ArgumentNullException">Occurs when the entered grid is null.</exception>
+    public static void LogGrid(string loggingTag, object[] grid, int height, int width, string separator = " ")
+    {
+        if (grid == null)
+            throw new ArgumentNullException("Received grid has a null value");
+        if (height <= 0)
+            throw new ArgumentOutOfRangeException(string.Format("Unexpected value of height, expected a positive value, received {0}.", height));
+        if (width <= 0)
+            throw new ArgumentOutOfRangeException(string.Format("Unexpected value of width, expected a positive value, received {0}.", width));
+        for (int row = 0; row < height; row++)
+            Debug.Log(loggingTag + " " + string.Join(separator, Enumerable.Range(row * height, width).Select(x => grid[x].ToString()).ToArray()));
+    }
+    /// <summary>
+    ///     Decomposes a rectangular grid of integers into multiple rows and sends each as a log message such that the logging is compatible with the Logfile Analyzer.<br></br>Each integer is indexed into <paramref name="itemSet"/> to obtain a string to represent each integer.
+    /// </summary>
+    /// <param name="loggingTag">The module's display name and moduleId, formatted as "[{displayName} #{moduleId}]"</param>
+    /// <param name="grid">The integer grid to be logged. The grid must be rectangular.</param>
+    /// <param name="height">The number of rows in the grid. The output will consist of this many messages.</param>
+    /// <param name="width">The number of items per row of the grid.</param>
+    /// <param name="itemSet">The set of objects to index each entry of <paramref name="grid"/> into.</param>
+    /// <param name="separator">A string to separate each entry of the grid</param>
+    /// <param name="offset">A value added to each value of <paramref name="grid"/> before it is indexed into <paramref name="itemSet"/>.</param>
+    /// <exception cref="ArgumentOutOfRangeException">Occurs when the stated width or height of the grid is 0 or fewer.</exception>
+    /// <exception cref="ArgumentNullException">Occurs when the entered grid is null.</exception>
+    public static void LogIntegerGrid(string loggingTag, int[] grid, int height, int width, object[] itemSet, string separator = " ", int offset = 0)
+    {
+        if (grid == null)
+            throw new ArgumentNullException("Received grid has a null value");
+        if (height <= 0)
+            throw new ArgumentOutOfRangeException(string.Format("Unexpected value of height, expected a positive value, received {0}.", height));
+        if (width <= 0)
+            throw new ArgumentOutOfRangeException(string.Format("Unexpected value of width, expected a positive value, received {0}.", width));
+        grid = grid.Select(x => x + offset).ToArray();
+        if (grid.Any(x => x < 0))
+            throw new IndexOutOfRangeException(string.Format("Unexpected value {0} found within the grid after applying offset. Values cannot be less than 0.", 
+                grid.First(x => x < 0)));
+        if (grid.Any(x => x >= itemSet.Count()))
+            throw new IndexOutOfRangeException(string.Format("Unexpected value {0} found within the grid after applying offset. Values cannot be greater than 1 fewer than the length of charSet ({1}).",
+                grid.First(x => x >= itemSet.Count()), itemSet.Count()));
+        
+        for (int row = 0; row < height; row++)
+            Debug.Log(loggingTag + " " + string.Join(separator, Enumerable.Range(row * height, width).Select(x => itemSet[grid[x] + offset].ToString()).ToArray()));
+    }
+
 }
