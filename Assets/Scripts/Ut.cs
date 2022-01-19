e4cbe966f886d1f94f6db83dd5e998c77168a0cf
Danny7007 <dannypignuola@gmail.com>
2022-01-18 20:42:11 -0500

    Add Replace and Swap methods

--- Assets/Scripts/Ut.cs
+++ Assets/Scripts/Ut.cs
@@ -85,6 +85,124 @@ public static class Ut
         var ordered = collection.OrderBy(comparer);
         return collection.First(x => x.Equals(ordered.First()));
     }
+    /// <summary>
+    ///     Returns a modified form of <paramref name="source"/> with all instances of <paramref name="original"/> replaced with <paramref name="substitute"/>.
+    /// </summary>
+    public static IEnumerable<T> Replace<T>(this IEnumerable<T> source, T original, T substitute)
+    {
+        if (source == null)
+            throw new ArgumentNullException("source");
+        var iter = source.GetEnumerator();
+        while (iter.MoveNext())
+        {
+            if (iter.Current.Equals(original))
+                yield return substitute;
+            else yield return iter.Current;
+        }
+    }
+    /// <summary>
+    ///     Returns a modified form of <paramref name="source"/>. All elements which have are a key of <paramref name="replacements"/> will be replaced with their corresponding value in <paramref name="replacements"/>.
+    /// </summary>
+    public static IEnumerable<T> Replace<T>(this IEnumerable<T> source, IDictionary<T, T> replacements)
+    {
+        if (source == null || replacements == null)
+            throw new ArgumentNullException(source == null ? "source" : "replacements");
+        var iter = source.GetEnumerator();
+        while (iter.MoveNext())
+        {
+            T cur = iter.Current;
+            if (replacements.ContainsKey(cur))
+                yield return replacements[cur];
+            else yield return cur;
+        }
+    }
+    /// <summary>
+    ///     Returns a modified form of <paramref name="source"/> in which all elements that return true in <paramref name="condition"/> are replaced with <paramref name="replacement"/>.
+    /// </summary>
+    public static IEnumerable<T> Replace<T>(this IEnumerable<T> source, Func<T, bool> condition, T replacement)
+    {
+        if (source == null || condition == null)
+            throw new ArgumentNullException(source == null ? "source" : "condition");
+        var iter = source.GetEnumerator();
+        while (iter.MoveNext())
+        {
+            T cur = iter.Current;
+            if (condition(cur))
+                yield return replacement;
+            else yield return cur;
+        }
+    }
+    /// <summary>
+    ///     Returns a modified form of <paramref name="source"/> in which all elements that return true by <paramref name="condition"/> are replaced by their result when inputted into <paramref name="replacementSelector"/>.
+    /// </summary>
+    public static IEnumerable<T> Replace<T>(this IEnumerable<T> source, Func<T, bool> condition, Func<T, T> replacementSelector)
+    {
+        if (source == null || condition == null || replacementSelector == null)
+            throw new ArgumentNullException(source == null ? "source" : condition == null ? "condition" : "replacementSelector");
+        var iter = source.GetEnumerator();
+        while (iter.MoveNext())
+        {
+            T cur = iter.Current;
+            if (condition(cur))
+                yield return replacementSelector(cur);
+            else yield return cur;
+        }
+    }
+    /// <summary>
+    ///     Replaces all elements equal to <paramref name="original"/> in <paramref name="source"/> with <paramref name="substitute"/>.
+    /// </summary>
+    public static void ReplaceInPlace<TSource, TElem>(this TSource source, TElem original, TElem substitute) where TSource : IList<TElem>
+    {
+        if (source == null)
+            throw new ArgumentNullException("source");
+        for (int i = 0; i < source.Count; i++){
+            if (source[i].Equals(original))
+                source[i] = substitute;
+        }
+    }
+    /// <summary>
+    ///     Replaces each element of <paramref name="source"/> which has a key in <paramref name="replacements"/> by its corresponding value in <paramref name="replacements"/>.
+    /// </summary>
+    public static void ReplaceInPlace<TSource, TElem>(this TSource source, IDictionary<TElem, TElem> replacements) where TSource : IList<TElem>
+    {
+        if (source == null || replacements == null)
+            throw new ArgumentNullException(source == null ? "source" : "replacements");
+        for (int i = 0; i < source.Count; i++)
+            if (replacements.ContainsKey(source[i]))
+                source[i] = replacements[source[i]];
+    }
+    /// <summary>
+    ///     Replaces each element of <paramref name="source"/> which returns true in <paramref name="condition"/> with <paramref name="replacement"/>.
+    /// </summary>
+    public static void ReplaceInPlace<TSource, TElem>(this TSource source, Func<TElem, bool> condition, TElem replacement) where TSource : IList<TElem>
+    {
+        if (source == null || condition == null)
+            throw new ArgumentNullException(source == null ? "source" : "condition");
+        for (int i = 0; i < source.Count; i++)
+            if (condition(source[i]))
+                source[i] = replacement;
+    }
+    /// <summary>
+    ///     Replaces each element of <paramref name="source"/> which returns true in <paramref name="condition"/> with the result of it being inputted into <paramref name="replacementSelector"/>.
+    /// </summary>
+    public static void ReplaceInPlace<TSource, TElem>(this TSource source, Func<TElem, bool> condition, Func<TElem, TElem> replacementSelector) where TSource : IList<TElem>
+    {
+        if (source == null || condition == null)
+            throw new ArgumentNullException(source == null ? "source" : condition == null ? "condition" : "replacementSelector");
+        for (int i = 0; i < source.Count; i++)
+            if (condition(source[i]))
+                source[i] = replacementSelector(source[i]);
+    }
+    /// <summary>
+    ///     Swaps the two elements in positions <paramref name="p1"/> and <paramref name="p2"/> in <paramref name="source"/>.
+    /// </summary>
+    public static void SwapPositions<T>(this IList<T> source, int p1, int p2)
+    {
+        T temp = source[p1];
+        source[p1] = source[p2];
+        source[p2] = temp;
+    }
+
 
     /// <summary>
     ///     Converts an integer number <paramref name="input"/> into a string representing its ordinal form.
@@ -158,34 +276,6 @@ public static class Ut
     }
 
     /// <summary>
-    /// Sets a given component of <paramref name="vect"/> to <paramref name="value"/>. <paramref name="component"/> must be equal to X, Y, Z, x, y or z.
-    /// </summary>
-    /// <param name="vect">The vector to be modified.</param>
-    /// <param name="component">The component of the vector to be modified. Must be X, Y, Z, x, y or z.</param>
-    /// <param name="value">The value to set the component to.</param>
-    /// <exception cref="ArgumentOutOfRangeException"></exception>
-    public static void SetComponent(this Vector3 vect, char component, float value)
-    {
-        if (vect == null)
-            throw new ArgumentNullException("vect");
-        switch (component)
-        {
-            case 'X':
-            case 'x':
-                vect.Set(value, vect.y, vect.z);
-                break;
-            case 'Y':
-            case 'y':
-                vect.Set(vect.x, value, vect.z);
-                break;
-            case 'Z':
-            case 'z':
-                vect.Set(vect.x, vect.y, value);
-                break;
-            default: throw new ArgumentOutOfRangeException("component", string.Format("Given component name ({0}) does match any of the components of the vector (x/y/z).", component));
-        }
-    }
-    /// <summary>
     ///     Linearly interpolates from <paramref name="start"/> to <paramref name="end"/> while <paramref name="t"/> is less than 0.5, and then interpolates backwards from <paramref name="end"/> to <paramref name="start"/> while <paramref name="t"/> is greater than 0.5.
     /// </summary>
     /// <param name="start">The initial value to be interpolated. This value will be equal to the output when <paramref name="t"/> == 0 or <paramref name="t"/> == 1.</param>
@@ -231,14 +321,15 @@ public static class Ut
     /// <summary>
     ///     Decomposes a rectangular grid into multiple rows and sends each as a log message such that the logging is compatible with the Logfile Analyzer.
     /// </summary>
-    /// <param name="loggingTag">The module's display name and moduleId, formatted as "[{displayName} #{moduleId}]"</param>
+    /// <param name="displayName">The module's display name, as specified in KMBombModule.ModuleDisplayName"</param>
+    /// <param name="moduleIdNumber">A number unique among all instances of <paramref name="displayName"/>.</param>
     /// <param name="grid">The grid to be logged. The grid must be rectangular.</param>
     /// <param name="height">The number of rows in the grid. The output will consist of this many messages.</param>
     /// <param name="width">The number of items per row of the grid.</param>
     /// <param name="separator">A string to separate each entry of the grid</param>
     /// <exception cref="ArgumentOutOfRangeException">Occurs when the stated width or height of the grid is 0 or fewer.</exception>
     /// <exception cref="ArgumentNullException">Occurs when the entered grid is null.</exception>
-    public static void LogGrid(string loggingTag, object[] grid, int height, int width, string separator = " ")
+    public static void LogGrid(string displayName, int moduleIdNumber, object[] grid, int height, int width, string separator = " ")
     {
         if (grid == null)
             throw new ArgumentNullException("Received grid has a null value");
@@ -247,13 +338,13 @@ public static class Ut
         if (width <= 0)
             throw new ArgumentOutOfRangeException(string.Format("Unexpected value of width, expected a positive value, received {0}.", width));
         for (int row = 0; row < height; row++)
-            Debug.Log(loggingTag + " " + string.Join(separator, Enumerable.Range(row * height, width).Select(x => grid[x].ToString()).ToArray()));
+            Debug.LogFormat("[{0} #{1}] {2}", displayName, moduleIdNumber, string.Join(separator, Enumerable.Range(row * height, width).Select(x => grid[x].ToString()).ToArray()));
     }
     /// <summary>
     ///     Decomposes a rectangular grid of integers into multiple rows and sends each as a log message such that the logging is compatible with the Logfile Analyzer.<br></br>Each integer is indexed into <paramref name="itemSet"/> to obtain a string to represent each integer.
     /// </summary>
-    /// <param name="loggingTag">The module's display name and moduleId, formatted as "[{displayName} #{moduleId}]"</param>
-    /// <param name="grid">The integer grid to be logged. The grid must be rectangular.</param>
+    /// <param name="displayName">The module's display name, as specified in KMBombModule.ModuleDisplayName"</param>
+    /// <param name="moduleIdNumber">A number unique among all instances of <paramref name="displayName"/>.</param>
     /// <param name="height">The number of rows in the grid. The output will consist of this many messages.</param>
     /// <param name="width">The number of items per row of the grid.</param>
     /// <param name="itemSet">The set of objects to index each entry of <paramref name="grid"/> into.</param>
@@ -261,7 +352,7 @@ public static class Ut
     /// <param name="offset">A value added to each value of <paramref name="grid"/> before it is indexed into <paramref name="itemSet"/>.</param>
     /// <exception cref="ArgumentOutOfRangeException">Occurs when the stated width or height of the grid is 0 or fewer.</exception>
     /// <exception cref="ArgumentNullException">Occurs when the entered grid is null.</exception>
-    public static void LogIntegerGrid(string loggingTag, int[] grid, int height, int width, object[] itemSet, string separator = " ", int offset = 0)
+    public static void LogIntegerGrid(string displayName, int moduleIdNumber, int[] grid, int height, int width, object[] itemSet, string separator = " ", int offset = 0)
     {
         if (grid == null)
             throw new ArgumentNullException("Received grid has a null value");
@@ -276,9 +367,8 @@ public static class Ut
         if (grid.Any(x => x >= itemSet.Count()))
             throw new IndexOutOfRangeException(string.Format("Unexpected value {0} found within the grid after applying offset. Values cannot be greater than 1 fewer than the length of charSet ({1}).",
                 grid.First(x => x >= itemSet.Count()), itemSet.Count()));
-
         for (int row = 0; row < height; row++)
-            Debug.Log(loggingTag + " " + string.Join(separator, Enumerable.Range(row * height, width).Select(x => itemSet[grid[x] + offset].ToString()).ToArray()));
+            Debug.LogFormat("[{0} #{1}] {2}", displayName, moduleIdNumber, string.Join(separator, Enumerable.Range(row * height, width).Select(x => itemSet[grid[x] + offset].ToString()).ToArray()));
     }
     /// <summary>
     ///     Takes an IEnumerable <paramref name="collection"/> and cyclically shifts its entries to the right.<br></br>If the value of <paramref name="rightShift"/> is less than 0, <paramref name="collection"/> will be shifted to the left by its absolute value. 
@@ -287,13 +377,12 @@ public static class Ut
     /// <typeparam name="TElem">The type of the collection's items.</typeparam>
     /// <param name="collection">The collection to be shifted.</param>
     /// <param name="rightShift">The number of places to shift the collection right by. If this value is negative, the collection will be shifted <em>left</em> by its absolute value</param>
-    /// <returns></returns>
     public static T ShiftRight<T, TElem>(this T collection, int rightShift) where T : IEnumerable<TElem>
     {
         if (collection == null)
             throw new ArgumentNullException("collection unexpected null value");
         if (rightShift < 0)
-            return (T)collection.Skip(-1 * rightShift).Concat(collection.Take(-1 * rightShift));
+            return (T)collection.Skip(-rightShift).Concat(collection.Take(-rightShift));
         else return (T)collection.Skip(collection.Count() - rightShift).Concat(collection.Take(rightShift));
     }
 
@@ -303,7 +392,7 @@ public static class Ut
     /// <param name="collection">The collection to be split.</param>
     /// <param name="groupSize">The size of each resulting IEnumerable</param>
     /// <param name="ignoreThrow">If this value is true, a check for all groups being equal will be bypassed, allowing the final group to have a value less than the <paramref name="groupSize"/></param>
-    public static IEnumerable<IEnumerable<T>> SplitCount<T>(this IEnumerable<T> collection, int groupSize, bool ignoreThrow = false)
+    public static IEnumerable<IEnumerable<T>> Chunk<T>(this IEnumerable<T> collection, int groupSize, bool ignoreThrow = false)
     {
         if (collection == null)
             throw new ArgumentNullException("collection unexpected null value");
